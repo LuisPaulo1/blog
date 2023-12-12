8afbda9553d8ddaaf8f4f1ae28759b478ab1a088
LuisPaulo1 <csluispaulo@gmail.com>
2023-12-11 23:02:11 -0300

    Deixando a content Page Dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id": 1,
+    "id": "1",
     "photo": "https://picsum.photos/500/300",
     "title": "Content Title 1",
     "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
   },
   {
-    "id": 2,
+    "id": "2",
     "photo": "https://picsum.photos/500/300",
     "title": "Content Title 2",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+  },
+  {
+    "id": "3",
+    "photo": "https://picsum.photos/500/300",
+    "title": "Content Title 3",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
+  },
+  {
+    "id": "4",
+    "photo": "https://picsum.photos/500/300",
+    "title": "Content Title 4",
     "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum."
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit {  
-  photoCover: string = 'https://picsum.photos/1200/300';
-  contentTitle: string = 'Content Title';
-  contentDescription: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.';
+export class ContentComponent implements OnInit {
+  id: string | null = '0';
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      console.log(params.get('id'));
+    this.route.paramMap.subscribe(value => {
+      this.id = value.get('id');
     });
-  }  
+    this.setValuesToComponente(this.id);
+  }
+  
+  setValuesToComponente(id: string | null): void {
+    const result = dataFake.filter(item => item.id === id)[0];
+    this.photoCover = result.photo;
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+  }
 }
